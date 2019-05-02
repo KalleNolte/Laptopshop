84b9627ea5bf284bd53d8713aef7fbe2c4e14130
Boby024 <william.kana-tsoplefack@stud.uni-due.de>
2019-05-02 19:21:18 +0200

    i make a mini blog with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { MaterialModule } from './material.module';
+import { AppComponent } from "./app.component"
+
+import { HomeComponent } from './home/home.component';
+import { AppareilComponent} from './appareil/appareil.component';
+import { from } from 'rxjs';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [AppComponent, HomeComponent, AppareilComponent, BlogComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
