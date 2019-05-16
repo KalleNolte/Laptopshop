4155a2e1ada06c405b5cea1430b2005c6ffd9e32
SherifNeamatalla <sherif.neamatalla@heuremo.de>
2019-05-16 13:38:56 +0200

    Added full binary_search support

--- backend/main_binary.py
+++ backend/main_binary.py
@@ -4,55 +4,78 @@ import skfuzzy as fuzz
 import numpy as np
 from collections import Counter
 import json
-
-from backend.binaryFunctions import binary_search
+from collections import defaultdict
 
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 app = Flask(__name__) #Create the flask instance, __name__ is the name of the current Python module
-priceDict = {}
-hdDict = {}
-productTitleDict = {}
-allDocs ={}
-
-# @app.route('/search')
-# def index():
-#     return render_template('main.html')
-
-
-@app.route('/search', methods=['POST'])
-def search():
-
-    minPrice = request.form['minPrice']
-    maxPrice = request.form['maxPrice']
-    hardDrive = request.form['hardDrive']
-
-    resBinary = binary_search.BinarySearch.computeBinary(es, minPrice, maxPrice, hardDrive)
-    resList = [dict(x) for x in (resBinary, )]
-
-
-    #Counter objects count the occurrences of objects in the list...
-    #count_dict contains each object from resList, and sums the scores for all occurrences. So if asin B07.. occurs
-    # twice with a score of 1.0, then it has a score of 2.0 in count_dict
-    count_dict = Counter()
-    for tmp in resList:
-        count_dict += Counter(tmp)
-
-    # print("count_dict")
-    # print(count_dict)
-    # print("dict(count_dict).items()")
-    # print(dict(count_dict).items()) #contains pairs
-
-    #count_dict is a dictionary of key/value pairs. Below is a list comprehension (for dictionaries?) to get key k and value v from each pair (k,v)
-    outputProducts = [("ASIN: {}".format(k), "Product Title: {}".format(productTitleDict[k]), "Vagueness Score: {}".format(v/2),
-                       "Price: {}".format(priceDict[k]), "Hard Drive Size: {}".format(hdDict[k])) for k, v in dict(count_dict).items()]
-
-    outputProducts = sorted(outputProducts, key=lambda x: x[2], reverse=True)
-
-    return jsonify(outputProducts) #original from alfred
 
 
 
+@app.route('/api/search', methods=['POST'])
+def searchBinary():
+    data = request.get_json()
+    query = createBinarySearchQuery(data)
+    res = es.search(index="amazon", body=query)
+    #print(res)
+    return jsonify(res)
+
+
+
+def createBinarySearchQuery(fieldNameToValueDict) :
+    body = defaultdict(lambda: defaultdict(lambda : defaultdict(lambda: defaultdict(dict))))
+
+    terms = []
+    ranges = []
+    sameFieldMultpleValues = []
+
+    for fieldName in fieldNameToValueDict :
+
+        #normal match
+        #Ranged terms, example : ram : { minRam : 2,maxRam : 4}
+        #If that's the case, then search for minRam and maxRam in fieldNameToValueDict, get them and add range to the query
+        if type(fieldNameToValueDict[fieldName]) is dict:
+            #Extract name of field, and set the name of min and max values to minField and maxField, example : minRam and maxRam.
+            #minValueName = "min"+fieldName[0].upper()+fieldName[1:]
+            #maxValueName = "max"+fieldName[0].upper()+fieldName[1:]
+
+            #Extract the values of minField and maxField from the JSON coming from the front end
+            minValue = fieldNameToValueDict[fieldName]["minValue"]
+            maxValue = fieldNameToValueDict[fieldName]["maxValue"]
+
+            ranges.append({"range" : {fieldName : {"lte" : maxValue,"gte" : minValue }}})
+
+        #--------------------------------------------------------------------------------------------------------------------------------#
+        #In case of multiple values for the same field, example : ram : [2,4,6], ram is either 2, 4 or 6.
+        elif type(fieldNameToValueDict[fieldName]) is list :
+            if type(fieldNameToValueDict[fieldName][0]) is str :
+                fieldNameToValueDict[fieldName] = [x.lower() for x in fieldNameToValueDict[fieldName]]
+            sameFieldMultpleValues.append({"terms" : {fieldName :fieldNameToValueDict[fieldName] }})
+        #--------------------------------------------------------------------------------------------------------------------------------#
+        #A normal numerical match, example : ram : 8, ram is 8
+        elif type(fieldNameToValueDict[fieldName]) is int or type(fieldNameToValueDict[fieldName]) is float :
+            terms.append({"term" : {fieldName : fieldNameToValueDict[fieldName]}})
+        #--------------------------------------------------------------------------------------------------------------------------------#
+        #A normal string match as brandName or hardDriveType
+        else :
+            #Example : match "{ ram : 8}"
+            terms.append({"term" : {fieldName : fieldNameToValueDict[fieldName].lower()}})
+        #--------------------------------------------------------------------------------------------------------------------------------#
+
+    body["query"]["bool"]["must"] = []
+    if len(terms) > 0 :
+        body["query"]["bool"]["must"].append(terms)
+    if len(ranges) > 0 :
+        body["query"]["bool"]["must"].append(ranges)
+    if len(sameFieldMultpleValues) > 0 :
+        body["query"]["bool"]["must"].append(sameFieldMultpleValues)
+
+    body.update({"size":100})
+
+    print(json.dumps(body))
+
+
+    return body
 
 
 if __name__ == "__main__":
