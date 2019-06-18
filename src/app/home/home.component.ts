e1fb4603f87cc84da37e69f76132233b09773121
Karl <karlphilippnolte@yahoo.de>
2019-06-18 11:29:52 +0200

    finished reactive forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,15 +35,114 @@ export class HomeComponent implements OnInit, AfterViewInit {
     { id: "dell", name: "DELL" }
   ];
 
-  manufacturers = [{ id: "intel", name: "Intel" }, { id: "amd", name: "AMD" }];
+  chipsetBrands = [{ id: "amd", name: "AMD" }, { id: "intel", name: "Intel" }];
+
+  screenSizes = [
+    { id: 1, name: "< 25 cm (10'')" },
+    { id: 2, name: "28 - 30 cm (11''-12'')" },
+    { id: 3, name: "33 - 36 cm (13''-14'')" },
+    { id: 4, name: "38 - 41 cm (15''-16'')" },
+    { id: 5, name: "> 43 cm (17'')" }
+  ];
+
+  processorTypes = [
+    { id: 2, name: "2 Cores" },
+    { id: 4, name: "4 Cores" },
+    { id: 6, name: "6 Cores" },
+    { id: 8, name: "8 Cores" }
+  ];
+
+  processorSpeeds = [
+    { id: 2, name: "< 2 GHz" },
+    { id: 3, name: "< 3 GHZ" },
+    { id: 4, name: "< 4 GHz" },
+    { id: 5, name: "< 5 GHz" }
+  ];
+
+  hardDriveTypes = [
+    { id: "hdd", name: "HDD" },
+    { id: "ssd", name: "SSD" },
+    { id: "hybrid", name: "Hybrid" }
+  ];
+
+  hardDriveSizes = [
+    { id: 128, name: "up to 128 GB" },
+    { id: 256, name: "128 - 256 GB" },
+    { id: 512, name: "256 - 512 GB" },
+    { id: 1, name: "512 - 1 TB" },
+    { id: 2, name: "1 - 2 TB" }
+  ];
+
+  operatingSystems = [
+    { id: "windows", name: "Windows" },
+    { id: "macos", name: "MacOS" },
+    { id: "linux", name: "Linux" }
+  ];
+
+  ramOptions = [
+    { id: 2, name: "2 GB" },
+    { id: 4, name: "4 GB" },
+    { id: 8, name: "8 GB" },
+    { id: 16, name: "16 TB" },
+    { id: 32, name: "32 TB" }
+  ];
+
+  weights = [
+    { id: 1, name: "< 1 kg" },
+    { id: 2, name: "1 - 1.5 kg" },
+    { id: 3, name: "1.5 - 2 kg" },
+    { id: 4, name: "2 - 2.5 kg" },
+    { id: 5, name: "> 2.5 kg" }
+  ];
 
   widgetForm = this.fb.group({
-    brandSlider: ["1"],
-    brandNames: this.fb.array([]),
-    processorManufacturerSlider: ["1"],
-    processorManufacturers: this.fb.array([]),
+    brandName: this.fb.group({
+      brandNameValue: this.fb.array([]),
+      weight: [1]
+    }),
+    chipsetBrand: this.fb.group({
+      chipsetBrandValue: this.fb.array([]),
+      weight: [1]
+    }),
+    screenSize: this.fb.group({
+      screenSizeValue: this.fb.array([]),
+      weight: [1]
+    }),
+    processorType: this.fb.group({
+      processorTypeValue: this.fb.array([]),
+      weight: [1]
+    }),
+    processorSpeed: this.fb.group({
+      processorSpeedValue: this.fb.array([]),
+      weight: [1]
+    }),
+    processorCount: this.fb.group({
+      processorCountValue: this.fb.array([]),
+      weight: [1]
+    }),
+    hardDriveType: this.fb.group({
+      hardDriveTypeValue: this.fb.array([]),
+      weight: [1]
+    }),
+    hardDriveSize: this.fb.group({
+      hardDriveSizeValue: this.fb.array([]),
+      weight: [1]
+    }),
+    operatingSystem: this.fb.group({
+      operatingSystemValue: this.fb.array([]),
+      weight: [1]
+    }),
+    ramSize: this.fb.group({
+      ramSizeValue: this.fb.array([]),
+      weight: [1]
+    }),
+    weight: this.fb.group({
+      weightValue: this.fb.array([]),
+      weight: [1]
+    })
   });
-   globalForm = new FormGroup({
+
+  globalForm = new FormGroup({
     globalSearch: new FormControl()
   });
 
@@ -76,24 +175,20 @@ export class HomeComponent implements OnInit, AfterViewInit {
     return this.widgetForm.get("processorManufacturers") as FormArray;
   }
 
-  addBrandName() {
-    this.brandNames.push(this.fb.control("Acer"));
-  }
-
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  touchFormFields(formGroup: FormGroup) {
-    Object.keys(formGroup.controls).forEach(field => {
-      const control = formGroup.get(field);
-      if (control instanceof FormControl) {
-        control.markAsTouched({ onlySelf: true });
-      } else if (control instanceof FormGroup) {
-        this.touchFormFields(control);
-      }
-    });
-  }
+  // touchFormFields(formGroup: FormGroup) {
+  //   Object.keys(formGroup.controls).forEach(field => {
+  //     const control = formGroup.get(field);
+  //     if (control instanceof FormControl) {
+  //       control.markAsTouched({ onlySelf: true });
+  //     } else if (control instanceof FormGroup) {
+  //       this.touchFormFields(control);
+  //     }
+  //   });
+  // }
 
   submitFormControls(formGroup: FormGroup) {
     console.log(formGroup.controls);
@@ -119,12 +214,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
       .subscribe(laptops => (this.laptops = laptops));
   }
 
-  onChange(event, fieldName) {
-    let field = (<FormArray>this.widgetForm.get(fieldName)) as FormArray;
+  onChange(event, groupName, fieldName) {
+    let field = (<FormArray>(
+      this.widgetForm.controls[groupName].get(fieldName)
+    )) as FormArray;
 
     if (event.checked) {
       field.push(new FormControl(event.source.value));
-      this.widgetForm.get(fieldName).markAsTouched();
+      field.markAsTouched();
       console.log("Field:" + fieldName);
     } else {
       const i = field.controls.findIndex(x => x.value === event.source.value);
